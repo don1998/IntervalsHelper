57efc78d5390120140c08d64fd7d254ffbb27e01
Doneil Scotland <doneilscotland@Qualitys-MacBook-Pro.local>
2020-06-30 12:09:28 -0500

    Implemented majority of the functionality

--- public/background.js
+++ public/background.js
@@ -2,11 +2,18 @@ chrome.runtime.onInstalled.addListener(() => {
     
     let date = new Date();   /* Creates the date object and sets it to tomorrow at 2:30 */
     //date.setHours(24,0,0,0);
-    date.setHours(15,45,0,0);
-    console.log(Number(date));
-    chrome.storage.sync.set({intervalsAlarmTime: date}, function() { /* Sets the intervalsAlarmTime in chrome storage */
-        chrome.alarms.create('dailyIntervalsAlarm', { periodInMinutes: 1 , when: Number(date)}); /* Creates the alarm using date object from chrome storage */    
-    });   
+    date.setHours(16,30,0,0);
+    let dateNumber = Number(date);
+    chrome.alarms.create('dailyIntervalsAlarm', { periodInMinutes: 1440 , when: dateNumber}); /* Creates the alarm using date object from chrome storage */
+  
+    chrome.storage.sync.set({dailyIntervalsAlarmTime: dateNumber},function(){ /* Sets the intervalsAlarmTime in chrome storage */
+      console.log("Alarm time set in chrome storage");
+    })
+
+    chrome.storage.sync.set({alarmState: true}, function() { /** Sets the alarmState to true when extension is first installed */
+      console.log("Alarm State intialised to true");
+    });
+
   });
 
   chrome.alarms.onAlarm.addListener((alarm) => {
@@ -15,7 +22,7 @@ chrome.runtime.onInstalled.addListener(() => {
   });
   
   function displayReminder() {
-    let option = confirm("It's time to update your timesheet!\n Do you want to do this now?");
+    let option = confirm("It's time to update your timesheet!\nDo you want to do this now?");
     if (option == true) {
         window.open("https://qualityworkscg.intervalsonline.com/time/","_blank")
     } 
--- src/AlarmMenu.js
+++ src/AlarmMenu.js
@@ -5,6 +5,17 @@ import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import moment from 'moment';
+import TimePicker from 'react-time-picker';
+import DoneIcon from '@material-ui/icons/Done';
+import IconButton from '@material-ui/core/IconButton';
+import AddAlarmIcon from '@material-ui/icons/AddAlarm';
+import AlarmOffIcon from '@material-ui/icons/AlarmOff';
+import AlarmOnIcon from '@material-ui/icons/AlarmOn';
+import ToggleOnOutlinedIcon from '@material-ui/icons/ToggleOnOutlined';
+import ToggleOffOutlinedIcon from '@material-ui/icons/ToggleOffOutlined';
+import AccessAlarmsIcon from '@material-ui/icons/AccessAlarms';
+import TextField from '@material-ui/core/TextField';
 
 
 class AlarmMenu extends React.Component{
@@ -13,23 +24,143 @@ class AlarmMenu extends React.Component{
         super(props);
         this.state = {
           classes: this.makeStyles(),
-          onButton:true,
-          offButton:false
+          alarmState:null,
+          currentAlarmTime:null,
+          setAlarmClicked:false,
+          timePicker:null
         };
       }
       
+    /**
+     * Orchestrates the toggling of the alarm button
+     */  
     toggleAlarm(){
-      this.setState({onButton:!this.state.onButton, offButton:!this.state.offButton});
-      if(this.state.onButton){ 
-        chrome.storage.sync.get(['intervalsAlarmTime'], function(result) {
-          alert(Number(result));
-          chrome.alarms.create('dailyIntervalsAlarm', { periodInMinutes: 1 , when: Number(result)});
-        });
+      if(this.state.alarmState == true){
+        
+        chrome.storage.sync.set({alarmState: false}, function() {  //turning alarm off
+          this.setState({alarmState:false});
+          
+          chrome.alarms.clear("dailyIntervalsAlarm", function (){
+
+          });
+
+          chrome.storage.sync.set({dailyIntervalsAlarmTime: null}, function(){
+            this.setState({currentAlarmTime:null});
+          }.bind(this));
+
+        }.bind(this));
+
       }
-      else if(this.state.offButton){
-        chrome.alarms.clear('dailyIntervalsAlarm')
+      else if(this.state.alarmState == false){
+
+        chrome.storage.sync.set({alarmState: true}, function() {   //turning alarm on
+          this.setState({alarmState:true});
+        }.bind(this));
+
+      }      
+    }
+
+
+    /**
+     * 
+     * @param {String} time - A string in the format HH:MM
+     * @returns {Date} - A javascript date object
+     */
+    convertTimetoDate(time){
+      let splitTime = time.split(":");
+      let hour = splitTime[0];
+      let minutes = splitTime[1];
+      let date = new Date();   
+      date.setHours(hour,minutes,0,0);
+      return date;
+    }
+
+    /**
+     * Converts javascript dates to momentjs dates for ease of use
+     * @param {Date} date - A javascript date object
+     */
+    convertDateToMoment(date){
+      let time = moment(date).format("hh:mm A")
+      return time
+    }
+
+    /**
+     * Parses the current alarm time to a more readable form or displays Alarm not Set
+     */
+    parseCurrentAlarmTime(){
+      if (this.state.currentAlarmTime == null || this.state.currentAlarmTime == undefined){
+        return "Alarm not Set"
+      }
+      else{
+        let time = this.convertDateToMoment(this.state.currentAlarmTime);
+        return time
+      }
+
+    }
+    /**
+     * Orchestrates the setting of the alarm
+     */
+    setAlarm(){
+      if(this.state.timePicker == undefined || this.state.timePicker == null){
+        alert("Please select a valid time");
+      }
+
+      let option = window.confirm(`Your alarm has been set for: ${this.state.timePicker} \nIs this fine?`);
+      
+      if (option==true){
+
+        let time = this.convertTimetoDate(this.state.timePicker);
+        
+        if(this.state.alarmState == true){
+          chrome.alarms.clear("dailyIntervalsAlarm", function (){}); /* Clears chrome alarm event if one already exists */
+          chrome.alarms.create('dailyIntervalsAlarm', { periodInMinutes: 1440 , when: Number(time)}); /* Creates chrome alarm event */
+          chrome.storage.sync.set({dailyIntervalsAlarmTime: Number(time)},function(){ /* Sets the intervalsAlarmTime in chrome storage */
+            this.setState({currentAlarmTime:time});
+          }.bind(this));
+        }
+        else if(this.state.alarmState == false){
+          chrome.storage.sync.set({dailyIntervalsAlarmTime: Number(time)},function(){ /* Sets the intervalsAlarmTime in chrome storage */
+            this.setState({currentAlarmTime:time});
+          }.bind(this));         
+        }
+        this.displaySetAlarmMenuItem();
+      }
+      else{
+        alert("Ok, please enter your desired time");
       }
     }
+
+
+    displayTimePicker(){
+      this.setState({setAlarmClicked:true});
+    }
+
+    displaySetAlarmMenuItem(){
+      this.setState({setAlarmClicked:false});
+    }
+
+
+    /**
+     * Sets the state with the value from the time picker element as the value changes
+     * @param {Object} timePicker 
+     */
+    onTimePickerChange = timePicker => {
+      this.setState({ timePicker: timePicker.target.value });
+    }
+
+
+
+    componentDidMount(){
+      chrome.storage.sync.get('alarmState', function(result) {  //Gets alarm state from chrome storage each time the extension is opened and sets it in the state
+        this.setState({alarmState:result.alarmState});
+      }.bind(this));
+      
+      chrome.storage.sync.get('dailyIntervalsAlarmTime', function(result){  //Gets alarm time from chrome storage each time the extension is opened and sets it in the state
+        this.setState({currentAlarmTime:result.dailyIntervalsAlarmTime})
+      }.bind(this));
+    }
+
+
 
     render(){
         return (
@@ -37,9 +168,32 @@ class AlarmMenu extends React.Component{
             <div className={this.state.classes.root}>
                 <Paper className={this.state.classes.paper}>
                 <MenuList>
-                    {this.state.onButton ? <MenuItem style={{color:'green'}}>Alarm is ON</MenuItem> : <MenuItem style={{color:'red'}}>Alarm is OFF</MenuItem>}
-                    <MenuItem onClick={()=>this.toggleAlarm()}> Toggle Alarm </MenuItem>
-                    <MenuItem>Set Alarm</MenuItem>
+                    {this.state.alarmState ? 
+                    <MenuItem id="onMenuItem" divider={true} button={false}><AlarmOnIcon style={{ color: "green" }} className="menuItemIcon"/>Alarm is ON</MenuItem> 
+                    : <MenuItem id="offMenuItem" divider={true} button={false}><AlarmOffIcon color="secondary" className="menuItemIcon"/>Alarm is OFF</MenuItem>}
+                    
+                    <MenuItem className="menuItem" divider={true} button={false}> <AccessAlarmsIcon className="menuItemIcon"/> {this.parseCurrentAlarmTime()}</MenuItem>
+                    
+                    <MenuItem className="menuItem" divider={true} onClick={()=>this.toggleAlarm()}>{this.state.alarmState?<ToggleOnOutlinedIcon className="menuItemIcon"/>:<ToggleOffOutlinedIcon className="menuItemIcon"/>} Toggle Alarm </MenuItem>                   
+                    {this.state.setAlarmClicked ? <MenuItem className="menuItem" button={false}>
+                        
+                        <TextField
+                          id="time"
+                          type="time"
+                          //defaultValue="07:30"
+                          className={this.state.classes.textField}
+                          onChange={this.onTimePickerChange}
+                          InputLabelProps={{
+                            shrink: true,
+                          }}
+                          inputProps={{
+                            step: 300, // 5 min
+                          }}
+                        />
+                        <IconButton onClick={()=>this.setAlarm()} children={<DoneIcon/>}/>
+                      </MenuItem>
+                    : <MenuItem className="menuItem" onClick={()=>this.displayTimePicker()}> <AddAlarmIcon className="menuItemIcon"/> Set Alarm</MenuItem>}
+      
                 </MenuList>
                 </Paper>
             </div>
@@ -47,13 +201,23 @@ class AlarmMenu extends React.Component{
         );
     }
 
+
+    /**
+     * Handles some of the styling for the materialui components
+     */
     makeStyles(){
         const useStyles = makeStyles((theme) => ({
           root: {
             display: 'flex',
+            fontSize:'0.85rem'
           },
           paper: {
             marginRight: theme.spacing(2),
+          },
+          textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+            width: 200,
           },
         }));
         return useStyles
